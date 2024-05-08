89ea72bcd7eb6e837f0297aa58cb28ec2365a667
Gabriel <119896525+LucasGabrielfl0@users.noreply.github.com>
2024-05-08 09:52:22 -0300

    add extra plot for Simulink

--- Project_2_White_Box_Model/P2_Kit_MODEL.m
+++ Project_2_White_Box_Model/P2_Kit_MODEL.m
@@ -5,6 +5,7 @@ clear
 s=tf('s');
 
 %% Tf2 White Box model
+% Circuit Parameters
 C3=120e-9;
 C4=33e-9;
 
@@ -12,6 +13,7 @@ R7=120e3;
 R8=47e3;
 R11=47e3;
 
+% White Box Model:
 Zc3= 1/(C3*s);
 Zc4= 1/(C4*s);
 
@@ -25,10 +27,10 @@ Tf2= -Z2/Z1;
 % Delete useless variables
 clear Z1 Z2 Zc3 Zc4
 
-%% Tf3 White box Model
+%% Tf23 (Cascade of Tf2 and Tf3) White box Model
+% Circuit Parameters for Tf3
 R13= 1e6;
 R14= 1e6;
-
 
 Tf3= -R13/R14;
 
@@ -46,7 +48,7 @@ time_raw=Response_Data(:,1);
 
 % plot(time_raw, Response_raw, time_raw, Input_raw)
 
-%Remove Delay:
+% Remove Time before Step:
 Time_Offset=-0.38038;
 Offset_Index=find(time_raw==Time_Offset);
 
@@ -56,21 +58,36 @@ Response_exp=Response_raw(Offset_Index: end);
 time=time_raw(Offset_Index: end);
 time=time-time(1);
 
-%Remove Offset
+% Remove Offset
 Input_exp =Input_exp - Input_exp(1);
 Response_exp=Response_exp-Response_exp(1);
-plot(time, Response_exp, time, Input_exp)
 
+% Plot
+plot(time, Response_exp, time, Input_exp)
+title('Experimental data')
+grid on
+
+%% Simulink Circuit data
+close all
+Slink_Circuit=sim('P2_Kit_Circuit.slx');
+
+% Note: Knowing the input is a unitary step
+Response_sim=Slink_Circuit.Tf23_Response.signals.values;
+time_sim=Slink_Circuit.Tf23_Response.time;
+
+plot(time_sim, Response_sim)
+title('Circuit Reponse on Simulink')
+grid on
 
 %% Comparing Model and Experimental Data:
 close all
 
 hold on
 
-plot(time, Response_exp,'r')   % Experimental Response
-step(Tf_23);                                % Model's Response
+plot(time, Response_exp,'r')               % Experimental Response
+plot(time_sim, Response_sim,'-g')          % Simulink Circuit
+step(Tf_23, time);                         % Model's Response
 
-legend('Experimental Response', 'Model Response')
+legend('Experimental Response', 'Model Response','Simulink Circuit Response' )
 title(' Step Response: Model x Experimental')
 grid on
-
