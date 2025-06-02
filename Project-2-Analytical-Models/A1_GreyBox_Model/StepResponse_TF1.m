c408c7e7539aff6184b61a27c37059fc0a3bc123
LucasGabrielfl0 <lucasgabrielfl00@gmail.com>
2025-06-02 12:10:36 -0300

    Update project 2 simulation and file

--- Project-2-Analytical-Models/A1_GreyBox_Model/StepResponse_TF1.m
+++ Project-2-Analytical-Models/A1_GreyBox_Model/StepResponse_TF1.m
@@ -16,8 +16,7 @@ R1 = -R2/K;   % [Ohm]
 % Analytical Transfer Function (Low Pass Filter)
 Gp=K/(t*s+1);
 
-step(Gp)
-grid on
+[Response_tf, time_tf]= step(Gp,'g');
 
 
 %% COMPARE (UNITARY) STEP RESPONSE
@@ -30,17 +29,19 @@ load("StepResponse_Experimental/time_exp.mat");
 load("StepResponse_Simulink/Response_slink.mat");
 load("StepResponse_Simulink/time_slink.mat");
 
+
 % Plot and Compare
 close all
 hold on
 
-plot(time_exp   , Response_exp   ,'m',LineWidth=1)         % Experimental Response
-plot(time_slink , Response_slink ,'--b',LineWidth=1)       % Model Response [Simulink Circuit]
-step(Gp,'g')                                               % Model Response [Matlab TF]
+plot(time_exp   , Response_exp   ,'m'  , LineWidth=1)       % Experimental Response
+plot(time_tf    , Response_tf    ,'g'  , LineWidth=1)       % Model Response [Matlab TF]
+plot(time_slink , Response_slink ,'--b', LineWidth=2)       % Model Response [Simulink Circuit]
+
 
 % Plot Config
 grid on
 legend('Experimental Response','Models response [Simulink]', 'Models response [TF]')
 title('Compare Models x Experimental responses')
-xlabel('Voltage [V]')
-ylabel('Time [sec]')
+ylabel('Voltage [V]')
+xlabel('Time [sec]')
